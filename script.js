35430963c73920317d8b07c76dc11bc6fe5f3771
vishal880c <vishalkr3r@gmail.com>
2025-06-03 21:33:44 +0530

    Add files via upload
    calculator

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-let currentPlayer = "X";
-let arr= Array(9).fill(null);
+let screen = document.getElementById('screen');
+buttons =  document.querySelector('button');
+let screenValue = ' ';
+for(item of button){
+          item.addEventListener('click')  ,  (e)=>{
+               buttonText = e . target.innerText;
+               console.log('button text is ' ,  buttonText);
+               if(buttonText == 'X'  )  {
+                      buttonText= '*';
+                      screen.value += buttonText;
+                      screen.value = screenValue;
+               }
+               else if (buttonText = '=' )  {
+                     screen.value= screenValue;
+                }
+                 else if (buttonText == '=' ){
+                        screen.value = eval(screenValue);
+              }
+              else {
+                  screenValue+= buttonText;
+                  screen.value= screenValue;
+              }
 
-
-function checkWinner() {
-    if (
-        (arr[0] !== null&& arr[0]  && arr[1] == arr[1]) ||
-        (arr[3] !== null&& arr[3]  && arr[4] == arr[5]) ||
-        (arr[6] !== null&& arr[6]  && arr[7] == arr[8]) ||
-        (arr[0] !== null&& arr[0]  && arr[3] == arr[6]) ||
-        (arr[1] !== null&& arr[1]  && arr[4] == arr[7]) ||
-        (arr[2] !== null&& arr[2]  && arr[5] == arr[8]) ||
-        (arr[0] !== null&& arr[0]  && arr[4] == arr[8]) ||
-        (arr[2] !== null&& arr[2]  && arr[4] == arr[6])
-     )  {
-            document.Write('Winner is ${currentplayer} ');
-            return;
-    }
-        
-         if (!arr.some((e) => e === null) ){
-         document.write("It's a draw!");
-            return;
-        }
-}
-
-function handleClick(el) {
-    const id = Number(el.id);{
-    array[id] = currentPlayer;
-    el.innerText = currentPlayer; 
-    checkWinner();  
-    currentPlayer = currentPlayer === "X" ? "0" : "x"
-    }
-}
+          } 
+  }      
